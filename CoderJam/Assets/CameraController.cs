596bf5346c8eb088ef24ebd134af0c9ee7f1c694
JeanBon51 <julien.barande@gmail.com>
2020-04-14 10:12:02 +0200

    First version PlayerController and Camera

---
+++ CoderJam/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+    [SerializeField] private float distance = 0;
+    [SerializeField] private float height = 0;
+
+    private void Update()
+    {
+        //transform.eulerAngles = new Vector3(0,0,height);
+        transform.position = target.position;
+        transform.position -= Quaternion.Euler(height,0,0) * Vector3.forward * distance;
+        transform.LookAt(target);
+    }
+}
---
+++ CoderJam/Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    float hori;
+    float verti;
+    [SerializeField] float speed = 0;
+    public GameObject test;
+
+
+    [SerializeField] private float AngleSpeed;
+    private float AngleRot;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+            verti = 1;
+        else if (Input.GetKey(KeyCode.S))
+            verti = -1;
+        else
+            verti = 0;
+        if (Input.GetKey(KeyCode.D))
+            hori = 1;
+        else if (Input.GetKey(KeyCode.Q))
+            hori = -1;
+        else
+            hori = 0;
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Instantiate(test, transform.position, transform.rotation);
+            transform.position = Vector3.zero;
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = ((Vector3.forward * verti + Vector3.right * hori).normalized * (speed * 1.0f / Time.fixedDeltaTime) * Time.fixedDeltaTime) + Vector3.up * rb.velocity.y;
+        
+
+        if (Mathf.Abs(rb.velocity.x) > 0.1 || Mathf.Abs(rb.velocity.z) > 0.1)
+        {
+            AngleRot = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, AngleRot, 0), Time.fixedDeltaTime * AngleSpeed);
+    }
+}
